b980f69e3d06b8b96c1006d4f5e724f441da78d4
arnabiam <arnabsarkar505@gmail.com>
2023-03-05 09:57:35 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,118 @@
+const mongoose = require('mongoose');
+
+
+//Conection and creating in  a new db
+mongoose.connect("mongodb://127.0.0.1:27017/ttchannel", {useNewUrlParser: true,
+useUnifiedTopology: true
+}).then(() => console.log("Connection Successful"))
+.catch((err) => console.log(err));
+
+//schema
+//A mongoose schema defines the structure of document, default values, validators, etc.
+
+const playlistSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        unique:true,
+        lowercase:true
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+//Collection Creation
+
+const Playlist = new mongoose.model("Playlist", playlistSchema);
+
+//create document or insert
+
+const createDocument = async ()=>{
+   try{
+    const reactPlaylist = new Playlist({
+        name: "javascript",
+        ctype: "Back End",
+        videos: 50,
+        author: "Thapa Technical",
+        active: true
+    })
+
+    const mongoPlaylist = new Playlist({
+        name: "MongoDB",
+        ctype: "DataBase",
+        videos: 50,
+        author: "Thapa Technical",
+        active: true
+    })
+    const expressPlaylist = new Playlist({
+        name: "Express JS",
+        ctype: "Back End",
+        videos: 50,
+        author: "Thapa Technical",
+        active: true
+    })
+    
+    // reactPlaylist.save();
+    // const result = await reactPlaylist.save();
+    const result = await Playlist.insertMany([reactPlaylist,mongoPlaylist,expressPlaylist])
+    console.log(result)
+   }catch(err){
+    console.log(err);
+   }
+    
+}
+createDocument();
+
+const getDocument = async ()=>{
+    // find({ctype: "Back End"})
+    // find({videos : {$gt : 20}})
+    //find({ctype : {$in : ['Back End', 'DataBase']}})
+    // find({$or : [{ctype:"Back End"} , {author: "Thapa Technical"}]})
+    //countDocuments()
+    //.sort("name : -1") will sort alphabetcally
+    //.sort({"name : -1"}) will sort insertwise 
+    try{
+        const resultt = await Playlist.find({$and : [{ctype:"Back End"} , {author: "Thapa Technical"}]})
+    .select({name:1,_id:0})
+    .limit(1);
+    console.log(resultt);
+}catch(err){
+   console.log(err)
+}
+}
+
+getDocument();
+
+// const updateDocument = async(id)=>{
+//     //finfByIdAndUpdate, useFindAndModify : false, new: true
+//     try{
+//         const result = await Playlist.updateOne({_id:id}, {$set : {
+//             name: "Javascript"
+//         }})
+//     } catch(err){
+//         console.log(err)
+//     }
+// }
+
+
+// updateDocument('6402f4e83cbba28f9aa845e0');
+
+const deleteDocument = async (_id) =>{
+
+    try{
+        
+            const result =  await Playlist.deleteOne({_id})
+        }
+     catch(err){
+        console.log(err)
+    }
+}
+
+
+deleteDocument('6402f4e83cbba28f9aa845e0');
